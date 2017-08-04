767da3433b940ed861916f78c852cfec2064f892
514687572 <514687572@qq.com>
2017-08-04 14:22:10 +0800

    Deeplearning4j demo
    Deeplearning4j demo

---
+++ src/main/java/com/stip/net/BeyondDl4jApplication.java
@@ -0,0 +1,12 @@
+package com.stip.net;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeyondDl4jApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeyondDl4jApplication.class, args);
+	}
+}
